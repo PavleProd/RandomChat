953a7dee4c9aec04cb976e3ae1302bde6eb75b71
p_prodanovic <pavleprod249@gmail.com>
2025-01-23 01:11:48 +0100

    chat window design overhaul part 1

--- ChatClient/MVVM/View/MainWindow.xaml.cs
+++ ChatClient/MVVM/View/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RandomChat
 {
@@ -21,14 +13,35 @@ namespace RandomChat
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged()
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
 
-        private void TextBox_TextChanged_1()
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
         {
-
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
         }
+
+        private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
     }
 }
